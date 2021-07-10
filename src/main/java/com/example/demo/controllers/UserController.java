9ed0edda7260f37bc17bb21e938b59d3bf6b18c5
Gustavo Antonio Lastra Colorado <gustavo.lastra@iteratec.com>
2021-07-10 10:00:20 +0200

    add UserController tests

---
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controllers;
+
+import com.example.demo.model.persistence.Cart;
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.CartRepository;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import com.example.demo.model.dto.CreateUserDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+	private final UserRepository userRepository;
+	private final CartRepository cartRepository;
+	private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public UserController(UserRepository userRepository, CartRepository cartRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+		this.userRepository = userRepository;
+		this.cartRepository = cartRepository;
+		this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+	}
+
+
+	@GetMapping("/id/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.of(userRepository.findById(id));
+	}
+
+	@GetMapping("/{username}")
+	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+		User user = userRepository.findByUsername(username);
+		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+	}
+
+	@PostMapping("/create")
+	public ResponseEntity<User> createUser(@RequestBody CreateUserDto createUserDto) {
+		User user = new User();
+		user.setUsername(createUserDto.getUsername());
+		Cart cart = new Cart();
+		cartRepository.save(cart);
+		user.setCart(cart);
+		if (createUserDto.getPassword().length()<7 || !createUserDto.getPassword().equals(createUserDto.getConfirmPassword()) ) {
+			return ResponseEntity.badRequest().build();
+		}
+		user.setPassword(bCryptPasswordEncoder.encode(createUserDto.getPassword()));
+		userRepository.save(user);
+		return ResponseEntity.ok(user);
+	}
+
+}
